349b3d1b8def25f7aa3071cd6815122d6ade0961
BuildTools <unconfigured@null.spigotmc.org>
2016-09-10 13:57:12 -0400

    Cubloc

--- ServerManager/src/com/cubloc/servermanager/commands/CommandsManager.java
+++ ServerManager/src/com/cubloc/servermanager/commands/CommandsManager.java
@@ -1,8 +1,11 @@
 package com.cubloc.servermanager.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+
 public class CommandsManager {
 
 	public void registerCommands() {
-
+		PluginManager manager = Bukkit.getPluginManager();
 	}
 }
